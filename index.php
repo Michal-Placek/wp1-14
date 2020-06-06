a957492e1c9f398ed3c2b33b692092e81b1e727f
Michal-Placek <michal.placek@student.ossp.cz>
2020-06-06 16:10:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,56 @@
 <?php
+
 $submit = filter_input(INPUT_POST, 'submit');
-$gender = filter_input(INPUT_POST, "gender");
-$email = filter_input(INPUT_POST, "email");
+$email = filter_input(INPUT_POST, 'email');
+$gender = filter_input(INPUT_POST, 'gender');
 
 ?>
- 
 <!DOCTYPE html>
-<html lang="cs">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-<?php
-if(isset($submit)) { ?>
-    <p>formulář byl odeslán a proto ho už znovu nezobrazuji!</p>
-    <p>Odeslaný email: xx@xx.cz</p>
-    <p>Zadané pohlaví: xxx</p>
-<?php
-} else { ?>
-    <p>stránka byla načtena bez odeslaného formuláře a proto zobrazuji formulář</p>
- 
-    <form action="index.php" method="post">
-        <label for="email">E-mail:</label>
-        <input type="email" name="email" id="email">
- 
-        <h2>Pohlaví</h2>
-        <label for="gender-male">Muž</label>
-        <input type="radio" name="gender" value="male" id="gender-male">
- 
-        <label for="gender-female">Žena</label>
-        <input type="radio" name="gender" value="female" id="gender-female">
-        
-        <label for="gender-none">Nespecifikováno</label>
-        <input type="radio" name="gender" value="none" id="gender-none">
-        <input type="submit" name="submit" value="Přidat do databáze">
 
-        </form>
-        <?php
+
+
+<?php
+
+    if(isset($submit)) {
+?> 
+
+    <h2>Formulář byl odeslán</h2>
+    Email: <?= $email ?></br>
+    Pohlaví: <?= $gender ?>
+
+<?php
+    } else { 
+?>
+    <form action="index.php" method="post">
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email">
+
+        <h2>Pohlaví</h2>
+        <input type="radio" name="gender" value="male" id="gender-male">
+        <label for="gender-male">Muž</label>
+        </br>
+
+        <input type="radio" name="gender" value="female" id="gender-female">
+        <label for="gender-female">Žena</label>
+        </br>
+
+        <input type="radio" name="gender" value="other" id="gender-other">
+        <label for="gender-other">Nespecifikováno</label>
+        </br>
+
+        <input type="submit" name="submit" value="Odeslat">
+    </form>
+
+   <h2>Stránka byla načtena bez formuláře</h2>
+   
+<?php
 }
 ?>
 
